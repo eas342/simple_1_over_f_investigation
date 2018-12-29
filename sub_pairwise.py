b7c8506a24948e3c4607ecc2a670d683b87921c4
Everett Schlawin <GranfalloonToyBalloon@hotmail.com>
2018-12-29 13:01:45 -0700

    added an argument to sub_pairwise to allow you to change the directory. Also, changed data type to 'int32' to allow subtraction (the unsigned 16 bit doesn't let you go negative)

--- sub_pairwise.py
+++ sub_pairwise.py
@@ -2,14 +2,26 @@ from astropy.io import ascii,fits
 import os
 import numpy as np
 import pdb
+import sys
+from sys import argv
 
-fullDir = '/data1/External/OTIS_unzipped/NRCNRCALONG-DARK-7235074213_1_1_924_JW1_JLAB40_20170823T074323.606_20170823T080253.912/NRCNRCALONG-DARK-72350742131_1_485_SE_2017-08-23T16h49m51.fits'
-pairWiseDir = 'pairwise_sub'
+if len(argv) >= 2:
+    if argv[1] == 'red':
+        print("Making subtractive pairs from reduced data...")
+        fullDir = '/data1/tso_analysis/otis_long_dark/proc/NRCNRCALONG-DARK-72350742131_1_485_SE_2017-08-23T16h49m51.red.fits'
+        pairWiseDir = 'pairwise_sub_red'
+    else:
+        print("Unrecognized argument. Returing ... ")
+        sys.exit()
+else:
+    fullDir = '/data1/External/OTIS_unzipped/NRCNRCALONG-DARK-7235074213_1_1_924_JW1_JLAB40_20170823T074323.606_20170823T080253.912/NRCNRCALONG-DARK-72350742131_1_485_SE_2017-08-23T16h49m51.fits'
+    pairWiseDir = 'pairwise_sub'
+
 overWrite = True
 
 fullDat = fits.open(fullDir)
 
-dat = fullDat['PRIMARY'].data
+dat = np.array(fullDat['PRIMARY'].data,dtype='int32')
 head = fullDat['PRIMARY'].header
 
 baseName = os.path.splitext(os.path.basename(fullDir))[0]
@@ -27,6 +39,7 @@ for oneGroup in np.arange(head['NGROUP'] - 1):
         fitsName = baseName + "_int_{:03d}_{:03d}.fits".format(oneGroup+1,oneGroup)
         imgHDU.header["ON_NINT"] = (oneGroup, "Which integration is the subtrahend")
         imgHDU.header["INT_NEXT"] = (oneGroup+1, "Which integration is the minuend")
+        imgHDU.header["INTTIME"] = (head['TFRAME'], "Total integration time for one fudged MULTIACCUM pairwise subtracted")
         outPath = os.path.join(pairWiseDir,fitsName)
         HDUList.writeto(outPath,overwrite=overWrite)
 
