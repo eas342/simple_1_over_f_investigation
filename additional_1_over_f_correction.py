64258a7c4e878ecd5b213cc473535be57e348837
Everett Schlawin <eas342@email.arizona.edu>
2019-05-18 14:38:42 -0700

    made a routine to use one amplifier to subtract against the others

--- additional_1_over_f_correction.py
+++ additional_1_over_f_correction.py
@@ -8,6 +8,17 @@ import glob
 from copy import deepcopy
 from scipy import ndimage
 
+"""
+Command line script to apply different forms of 1/f corrections to data
+These are intended to go beyond traditional reference pixel correction methods
+
+Usage
+python additional_1_over_f_correction.py Method
+where Method is 'rowColSub', 'smoothedRowKernel'
+refAmpFlip# where # is 0 through 3.
+If Method is not specified, it does rowSub or 'row-by-row subtraction'
+"""
+
 #redFile = 'proc/NRCNRCA3-DARK-72552035481_1_483_SE_2017-09-12T23h40m37.red.fits'
 redFile = 'proc/NRCNRCALONG-DARK-72350742131_1_485_SE_2017-08-23T16h49m51.red.fits'
 
@@ -22,6 +33,9 @@ if len(argv) > 1:
         outDir = 'proc_red_smoothedRowKernel'
         correctionMode = 'smoothedRowKernel'
         kernel = np.ones([1,windowSize])/np.float(windowSize)
+    elif 'refAmpFlip' in argv[1]:
+        outDir = 'proc_red_{}'.format(argv[1])
+        correctionMode = argv[1]
     else:
         print("unrecognized correction type")
         sys.exit()
@@ -55,6 +69,24 @@ for ind,oneGroup in enumerate(origDat):
         correctedDat = dat - smoothedImage
     elif correctionMode == 'rowSub':
         correctedDat = dat - correction2D
+    elif 'refAmpFlip' in correctionMode:
+        try:
+            refAmp = int(correctionMode[-1])
+        except ValueError:
+            print('No valid amplifier recognized in {}'.format(correctionMode))
+            sys.exit()
+        
+        refAmpImg = dat[:,refAmp * 512:(refAmp + 1) *512]
+        correctionImg = np.zeros_like(dat)
+        ampDirections = [1,-1,1,-1]
+        nAmp = 4
+        for oneAmp in np.arange(nAmp):
+            if ampDirections[oneAmp] == ampDirections[refAmp]:
+                orientedAmpImage = refAmpImg
+            else:
+                orientedAmpImage = np.flip(refAmpImg,axis=1)
+            correctionImg[:,oneAmp * 512:(oneAmp + 1) * 512] = orientedAmpImage
+        correctedDat = dat - correctionImg
     else:
         print("unrecognized correction mode")
         sys.exit()
