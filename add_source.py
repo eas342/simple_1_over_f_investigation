41ee23e27d8e671c9dbd20e3c0859b95d5fd13f2
Everett Schlawin <granfalloontoyballoon@hotmail.com>
2020-03-09 08:39:23 -0700

    add_source now can add to some lab data slopes as well

--- add_source.py
+++ add_source.py
@@ -7,8 +7,15 @@ import pdb
 import os
 
 def add_source(inputDir='pairwise_sub_red',
-               sourceToAdd='slope_NRCALONG_FULL_F322W2_sim.fits',custText=''):
-    fileL = glob.glob(os.path.join(inputDir,'NRC*.fits'))
+               sourceToAdd='slope_NRCALONG_FULL_F322W2_sim.fits',custText='',
+               slopeFiles=False):
+    
+    if slopeFiles == True:
+        searchName = 'NRC*.slp.fits'
+    else:
+        searchName = 'NRC*.fits'
+    
+    fileL = glob.glob(os.path.join(inputDir,searchName))
     
     sourceImgFile = os.path.join('sim_grism_slope',sourceToAdd)
     sourceImg = fits.getdata(sourceImgFile)
@@ -22,12 +29,17 @@ def add_source(inputDir='pairwise_sub_red',
     frameTime = 10.73677
     gain = 1.8
     
+    if slopeFiles == True:
+        sourceToAdd = sourceImg / gain ## convert to ADU
+    else:
+        sourceToAdd = sourceImg * frameTime / gain ## convert to ADU and add to counts
+    
     for ind,oneFile in enumerate(fileL):
         if np.mod(ind,10) == 0:
             print("Working on {} of {}".format(ind+1,len(fileL)))
         HDUList = fits.open(oneFile)
-        
-        HDUList[0].data = HDUList[0].data + sourceImg * frameTime / gain ## convert to ADU
+            
+        HDUList[0].data = HDUList[0].data + sourceToAdd
         
         outFileName = os.path.basename(oneFile)
         outFilePath = os.path.join(outDir,outFileName)
@@ -39,3 +51,8 @@ if __name__ == "__main__":
     add_source()
     
 
+def azlab_dark_sim():
+    """ Simulate grism image using AZ Lab subarray darks """
+    add_source(sourceToAdd='slope_NRCALONG_SUBGRISM256_F322W2_sim.fits',
+               inputDir='/fenrirdata1/es_tso/AZLab05/all_azlab05/proc/raw_separated_MMM_refpix/NRCBSELINEDARKNCOLS2048_1_487_S_2019-08-03T07h36m37',
+               slopeFiles=True,custText='_f322w2')
